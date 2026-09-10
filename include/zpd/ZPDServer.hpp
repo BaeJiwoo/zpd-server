b82fd23a01a6c30948eafe14203b7bd879f40b44
BaeJiwoo <zwoo0896@gmail.com>
2026-09-11 04:00:48 +0900

    refactor: remove display method

--- include/zpd/ZPDServer.hpp
+++ include/zpd/ZPDServer.hpp
@@ -2,7 +2,6 @@
 #define ZPD_ZPDSERVER_HPP
 
 #include "IOCPServer.hpp"
-#include <string>
 
 class ZPDServer final : public IOCPServer
 {
@@ -20,7 +19,9 @@ class ZPDServer final : public IOCPServer
     void OnReceived(std::uint32_t clientId, const char* data, DWORD size) override
     {
         std::cout << "[received] client=" << clientId << " bytes=" << size
-                  << " message=\"" << DisplayMessage(data, size) << '"' << std::endl;
+                  << " message=\"";
+        std::cout.write(data, size);
+        std::cout << '"' << std::endl;
         if (!Send(clientId, data, size)) {
             std::cerr << "[send failed] client=" << clientId << std::endl;
             Disconnect(clientId);
@@ -30,37 +31,14 @@ class ZPDServer final : public IOCPServer
     void OnSendCompleted(std::uint32_t clientId, const char* data, DWORD size) override
     {
         std::cout << "[sent] client=" << clientId << " bytes=" << size
-                  << " message=\"" << DisplayMessage(data, size) << '"' << std::endl;
+                  << " message=\"";
+        std::cout.write(data, size);
+        std::cout << '"' << std::endl;
     }
 
     void OnDisconnected(std::uint32_t clientId) override
     {
         std::cout << "[disconnected] client=" << clientId << std::endl;
-    }
-  private:
-    static std::string DisplayMessage(const char* data, DWORD size)
-    {
-        constexpr char hex[] = "0123456789ABCDEF";
-        std::string text;
-        for (DWORD i = 0; i < size; ++i) {
-            const auto value = static_cast<unsigned char>(data[i]);
-            if (value == '\n')
-                text += "\\n";
-            else if (value == '\r')
-                text += "\\r";
-            else if (value == '\t')
-                text += "\\t";
-            else if (value == '"' || value == '\\') {
-                text += '\\';
-                text += static_cast<char>(value);
-            } else if (value < 32 || value == 127) {
-                text += "\\x";
-                text += hex[value >> 4];
-                text += hex[value & 15];
-            } else
-                text += static_cast<char>(value);
-        }
-        return text;
     }
 };
 
